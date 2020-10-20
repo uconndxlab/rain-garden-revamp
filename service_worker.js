b3d86f55d997d88e366414e354e29ab5b87b7eb0
Harrison <harrison.burr@gmail.com>
2020-10-19 22:10:32 -0400

    fixing PWA service worker and manifest

--- service_worker.js
+++ service_worker.js
@@ -6,7 +6,7 @@ self.addEventListener('activate', e => {
   console.log('Service Worker has been activated');
 });
 
-const preCacheName = "pre-cache-hbp", preCacheFiles = [ "/", "css/general.css", "css/index.css", "pages/basics.html", "pages/choose_plants.html", "pages/design.html", "pages/install.html", "pages/video_tutorials.html", "js/jquery3.5.1.min.js", "js/index.js" ]; 
+const preCacheName = "pre-cache-hbp", preCacheFiles = [ "/", "pages/offline.html", "css/general.css", "css/index.css", "pages/basics.html", "pages/choose_plants.html", "pages/design.html", "pages/install.html", "pages/video_tutorials.html", "js/jquery3.5.1.min.js", "js/index.js" ];
 
 self.addEventListener("fetch", event => {
   event.respondWith(caches.match(event.request).then(response => {
